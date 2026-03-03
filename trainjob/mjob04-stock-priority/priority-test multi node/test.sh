b39c8ac1b40a4dfaa6bb7831d77181f131a19c38
oratia5702 <oratia5702@gmail.com>
2026-03-03 16:16:36 +0900

    테스트 shell 제작

---
+++ trainjob/mjob04-stock-priority/priority-test multi node/test.sh
@@ -0,0 +1,7 @@
+kubectl apply -f 01-resources.yaml
+kubectl apply -f 02-pv.yaml
+kubectl apply -f 03-secret.yaml
+kubectl apply -f 04-runtime.yaml
+kubectl apply -f 05-trainjob-low.yaml
+sleep 60
+kubectl apply -f 06-trainjob-high.yaml
