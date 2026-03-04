0279dc2a4011243d3808b164e6b8c3886fb6f522
oratia5702 <oratia5702@gmail.com>
2026-03-04 16:22:23 +0900

    sh 파일 수정 다이렉코리 수정

--- trainjob/mjob04-stock-mps/test.sh
+++ trainjob/mjob04-stock-mps/test.sh
@@ -2,4 +2,4 @@ kubectl apply -f 01-resources.yaml
 kubectl apply -f 02-pvc.yaml
 kubectl apply -f 03-secret.yaml
 kubectl apply -f 04-runtime.yaml
-kubectl apply -f 05-trainjob-low.yaml
+kubectl apply -f 05-trainjob.yaml
