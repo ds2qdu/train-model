2fafbd8cf9511acbf0c79896770c0a4cf7866417
jhchoi <jhchoi@clush.net>
2026-04-08 16:28:55 +0900

    - pre-define 사용으로 변경

--- trainjob/example/env.sh
+++ trainjob/example/env.sh
@@ -61,6 +61,8 @@ if [ "$RANK" != "0" ]; then
 fi
 
 torchrun \
+  --nnodes=$KUBE_NODE_SIZE \
+  --nproc_per_node=1 \
   --node_rank=$RANK \
   --master_addr=$MASTER_ADDR \
   --master_port=$MASTER_PORT \
