0b147f3b0693d5699178f9a01c6554c4fc068a3d
jhchoi <jhchoi@clush.net>
2026-04-13 13:23:00 +0900

    typo

--- trainjob/example/env.sh
+++ trainjob/example/env.sh
@@ -85,7 +85,7 @@ if [ "$RANK" != "0" ]; then
 fi
 
 torchrun \
-  --nnodes=$KUB_NODE_SIZE \
+  --nnodes=$KUBE_NODE_SIZE \
   --nproc_per_node=1 \
   --node_rank=$RANK \
   --master_addr=$MASTER_ADDR \
